817b4c77cb09eab0593aebe7ec93d5b834d0c6d8
Neelam <meritneelam2004@gmail.com>
2025-08-05 17:30:04 +0000

    Add username validation and fetch logic for LeetCode stats

--- script.js
+++ script.js
@@ -10,4 +10,49 @@ document.addEventListener("DOMContentLoaded", function() {
     const mediumLabel = document.getElementById("medium-label");
     const hardLabel = document.getElementById("hard-label");
     const cardStatsContainer = document.querySelector(".stats-card");
-})
+
+    function validateUsername(username){
+        if(username.trim() == ""){
+            alert("Username should not be empty");
+            return false;
+        }
+        const regex = /^[a-zA-Z0-9_-]{1,15}$/;  
+        const isMatching = regex.test(username);
+        if(!isMatching){
+            return alert("Invalid Username");
+        }
+        return isMatching;
+    }
+
+    async function fetchUserDetails(username){
+        const url = `https://leetcode-stats-api.herokuapp.com/${username}`
+        try{
+            searchButton.textContent = "Searching..."
+            searchButton.disabled = true;
+
+            const response = await fetch(url);
+            if(!response){
+                throw new Error("Unable to fetch the user details");
+            }
+
+            const data = await response.json();
+            console.log("Logging data:", data);
+        }
+        catch(error){
+            statsContainer.innerHTML = `<p>No data Found</p>`
+        }
+        finally{
+            searchButton.textContent = "Search";
+            searchButton.disabled = false;
+        }
+    }
+
+    searchButton.addEventListener('click', function() {
+        const username = userNameInput.value;
+        console.log("logging username:", username);
+        if(validateUsername(username)){
+            fetchUserDetails(username);
+        }
+    })
+});
+
